3ecd2a2b5ebc9eddffaf7861cb1ff38fd255e190
Aniruddha Sutradhar <aniruddhasutradhar@Aniruddhas-MacBook-Air.local>
2026-07-07 15:41:04 +0530

    Auto-download NLTK data on startup for deployment

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 app = Flask(__name__)
 
 stop_words = set(stopwords.words("english"))
